238e84737df5fd5d9fe7c2a6f458ccf34520437b
Nihal Abdul Khader MP <84581207+Nihalmp45@users.noreply.github.com>
2023-10-28 15:11:51 +0530

    new feautre started working

--- src/App.js
+++ src/App.js
@@ -1,52 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import { PieChart } from 'react-minimal-pie-chart';
 
-function App() {
-  const initialMinutes = 1; // 5 minutes
-  const [timeRemaining, setTimeRemaining] = useState(initialMinutes * 60); // Convert to seconds
-  const totalTime = initialMinutes * 60;
-
-  const startCountdown = () => {
-    if (timeRemaining > 0) {
-      setTimeRemaining(timeRemaining - 1);
-    }
-  };
-
-  const addTime = () => {
-    setTimeRemaining(timeRemaining + 10); // Allow infinite adjustment
-  };
-
-  const resetTime = () => {
-    setTimeRemaining(initialMinutes * 60);
-  };
+const App = () => {
+  const [timeRemaining, setTimeRemaining] = useState(300); // 5 minutes in seconds
+  const totalTime = 300; // 5 minutes in seconds
 
   useEffect(() => {
-    const timer = setInterval(startCountdown, 1000);
+    const timer = setInterval(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      }
+    }, 1000);
+
     return () => clearInterval(timer);
   }, [timeRemaining]);
 
-
-
+  const progress = (timeRemaining / totalTime) * 100;
 
   return (
-    <div className="App">
-      <div id="countdown-container">
-        <h3>Routine Starting in ...</h3>
-        <div id="timer">
-          <div id="progress-container">
-            <div id="outer-ring" ></div>
-            <div id="progress-bar">
-              <div className="time-remaining">
-                {new Date(timeRemaining * 1000).toISOString().substr(14, 5)}
-              </div>
-            </div>
-          </div>
-        </div>
-        <button onClick={addTime}>+ 10 seconds</button>
-        <button onClick={resetTime}>Reset</button>
-      </div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h3>Routine Starting in ...</h3>
+      <PieChart
+        data={[
+          { title: 'Time Remaining', value: progress, color: '#e80c25' },
+          { title: 'Remaining', value: 100 - progress, color: '#ccc' },
+        ]}
+        lineWidth={15}
+        style={{ width: '200px', height: '200px' }}
+      />
+      <div style={{ fontSize: '24px', fontWeight: 'bold' }}>{timeRemaining}</div>
+      <button onClick={() => setTimeRemaining(timeRemaining + 10)}>+ 10 seconds</button>
     </div>
   );
-}
+};
 
-export default App;
+export default App
